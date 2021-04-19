277c5a9dd01a521d0e86677ebd49a1f02df993c2
Maite Quiles Cerdà <o_o.maite@hotmail.com>
2021-04-19 20:50:53 +0200

    first commit

---
+++ test-rocket-ship-main/src/routes/LaunchPad/components/Rocket/components/Rocket.js
@@ -0,0 +1,27 @@
+import React, { useState, Component } from 'react';
+import RocketCore from './RocketCore';
+
+export function FunctionalRocket() {
+  const [initialLaunchTime] = useState(Date.now());
+
+  return <RocketCore initialLaunchTime={initialLaunchTime} />;
+}
+
+export class ClassRocket extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      initialLaunchTime: Date.now()
+    };
+  }
+  
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  render() {
+    const { initialLaunchTime } = this.state;
+    return <RocketCore initialLaunchTime={initialLaunchTime} />;
+  }
+}
