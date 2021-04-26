295c45c5bfb876b2f7f73b4e0bf1c8a06b7a5ca7
Maite Quiles Cerdà <o_o.maite@hotmail.com>
2021-04-26 09:57:27 +0200

    removed credentials from config file and added file to git

---
+++ test-spootify-main/src/config.js
@@ -0,0 +1,9 @@
+export default {
+  api: {
+    baseUrl: 'https://api.spotify.com/v1',
+    authUrl: 'https://accounts.spotify.com/api/token',
+    // get credentials on https://developer.spotify.com/dashboard/
+    clientId: '',
+    clientSecret: ''
+  }
+}
